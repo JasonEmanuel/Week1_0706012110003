13b2ab2e53fcb09d7c5c2ff4852cab80168d18a6
JasonEmanuel <jemanuel@student.ciputra.ac.id>
2022-09-18 21:42:11 +0700

    APK Farm

--- app/src/main/java/com/uc/farmvisprog_0706012110003/MainActivity.kt
+++ app/src/main/java/com/uc/farmvisprog_0706012110003/MainActivity.kt
@@ -22,6 +22,6 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
-        }, 4000)
+        }, 3000)
     }
 }
